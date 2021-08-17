abe89aecadc96de35300352cae2c9d0be1b8e122
fatihaaksoz <fatih.aksoz19@gmail.com>
2021-08-17 18:24:01 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NewsItem from "./NewsItem";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <NewsItem/>
+      <NewsItem/>
   </React.StrictMode>,
   document.getElementById('root')
 );
